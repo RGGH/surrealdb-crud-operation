e0289723b22de5a8e2659915a0c5dea923fa3956
ranafge <ranafge@gmail.com>
2024-03-13 01:26:29 +0600

    update and delete database query added

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ async fn main() -> surrealdb::Result<()> {
 
     db.use_ns("district").use_db("district").await?;
     
-    let shatkhira: Vec<Record> = db
+    let mithapukur: Vec<Record> = db
         .create("District")
         .content(District {
                 number_of_thana: 2,
@@ -40,17 +40,86 @@ async fn main() -> surrealdb::Result<()> {
                 police_station: vec!["mithapukur".to_string()]
         })
         .await?;
-    println!("{:?}", shatkhira);
+    println!("{:?}", mithapukur);
+//     [
+//     {
+//         "id": "District:hc7tvmarkgbj74cxjc4c",
+//         "number_of_thana": 2,
+//         "police_station": [
+//             "mithapukur"
+//         ],
+//         "population": 1000000
+//     }
+// ]
 
     // Update all records in a table
-//    let district: Vec<Record> = db.update("khulna")
-//      .content(Division {
-//             name: "Shatkhira".to_string(),
-//             climate: "Sunny".to_string(),
-//             population: 340000,
-//             international_boder: true,
-//         })
-//      .await?;
+   let mithapukur: Vec<Record> = db.update("District")
+     .content(District {
+            number_of_thana: 1,
+            population: 100002873,
+             police_station: vec!["mithapukur".to_string(), "Jaigirhat".to_string()]
+                     })
+     .await?;
+    println!("{:?}", mithapukur);
+    dbg!(mithapukur);
+
+    // update only single field ok
+    let query = r#"
+        UPDATE District
+        SET number_of_thana = 2
+        WHERE id="District:2v88h9zbdx9hqc9owr5i"
+    "#;
+    db.query(query).await?;
+
+    // update only vector field
+    let query = r#"
+        UPDATE District
+        SET police_station += ['shathibari']
+        WHERE id="District:2v88h9zbdx9hqc9owr5i"
+    "#;
+    db.query(query).await?;
+
+    // update only single with increment
+    let query = r#"
+        UPDATE District
+        SET number_of_thana = number_of_thana + 4
+        WHERE id="District:9j4uapk2bn56ix4yy7tm"
+    "#;
+    db.query(query).await?;
+
+    // update only single with decrement
+    let query = r#"
+        UPDATE District
+        SET number_of_thana = number_of_thana - 1
+        WHERE id="District:2v88h9zbdx9hqc9owr5i"
+    "#;
+    db.query(query).await?;
+
+    // update only single with increment
+       // update only vector field
+       let query = r#"
+       UPDATE District
+       SET police_station += ['shathibari', 'mirjapur', 'shukurhat']
+       WHERE id="District:2v88h9zbdx9hqc9owr5i"
+   "#;
+   db.query(query).await?;
+
+   // update number_of_thana according to lenght of police_station lenth
+//    let query = r#"
+//     UPDATE District
+//     SET number_of_thana = LENGTH(police_station)
+// "#;
+//     db.query(query).await?;
+    // delete record
+    let query = r#"
+        DELETE FROM District
+                    
+        WHERE id="District:2v88h9zbdx9hqc9owr5i"
+    "#;
+    db.query(query).await?;
+
+   
+    
 
     // Delete all records in a table
     // db.delete("khulna").await?;
