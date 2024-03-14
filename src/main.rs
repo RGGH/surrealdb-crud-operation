3d8bc53edae87e8fb847f5ad3c3cb10c5941da08
ranafge <ranafge@gmail.com>
2024-03-14 17:50:36 +0600

    array::len added return number of item of array

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,10 @@ struct Division {
 struct District {
     number_of_thana: usize,
     population: usize,
-    police_station: Vec<String>
+    police_station: Vec<String>,
 }
 
-#[derive(Debug,Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Record {
     id: Thing,
 }
@@ -31,35 +31,36 @@ async fn main() -> surrealdb::Result<()> {
     .await?;
 
     db.use_ns("district").use_db("district").await?;
-    
+
     let mithapukur: Vec<Record> = db
         .create("District")
         .content(District {
-                number_of_thana: 2,
-                population: 1000000,
-                police_station: vec!["mithapukur".to_string()]
+            number_of_thana: 2,
+            population: 1000000,
+            police_station: vec!["mithapukur".to_string()],
         })
         .await?;
     println!("{:?}", mithapukur);
-//     [
-//     {
-//         "id": "District:hc7tvmarkgbj74cxjc4c",
-//         "number_of_thana": 2,
-//         "police_station": [
-//             "mithapukur"
-//         ],
-//         "population": 1000000
-//     }
-// ]
+    //     [
+    //     {
+    //         "id": "District:hc7tvmarkgbj74cxjc4c",
+    //         "number_of_thana": 2,
+    //         "police_station": [
+    //             "mithapukur"
+    //         ],
+    //         "population": 1000000
+    //     }
+    // ]
 
     // Update all records in a table
-   let mithapukur: Vec<Record> = db.update("District")
-     .content(District {
+    let mithapukur: Vec<Record> = db
+        .update("District")
+        .content(District {
             number_of_thana: 1,
             population: 100002873,
-             police_station: vec!["mithapukur".to_string(), "Jaigirhat".to_string()]
-                     })
-     .await?;
+            police_station: vec!["mithapukur".to_string(), "Jaigirhat".to_string()],
+        })
+        .await?;
     println!("{:?}", mithapukur);
     dbg!(mithapukur);
 
@@ -96,30 +97,38 @@ async fn main() -> surrealdb::Result<()> {
     db.query(query).await?;
 
     // update only single with increment
-       // update only vector field
-       let query = r#"
+    // update only vector field
+    let query = r#"
        UPDATE District
        SET police_station += ['shathibari', 'mirjapur', 'shukurhat']
        WHERE id="District:2v88h9zbdx9hqc9owr5i"
    "#;
-   db.query(query).await?;
-
-   // update number_of_thana according to lenght of police_station lenth
-//    let query = r#"
-//     UPDATE District
-//     SET number_of_thana = LENGTH(police_station)
-// "#;
-//     db.query(query).await?;
-    // delete record
-    let query = r#"
-        DELETE FROM District
-                    
-        WHERE id="District:2v88h9zbdx9hqc9owr5i"
-    "#;
     db.query(query).await?;
 
-   
-    
+    // update a police_station filed to add another one as well as update number_of_thena
+    // x3we856001c2ibzweexz
+    let update_thana = r#"
+                     UPDATE District
+                     SET police_station += ['pirgacha','pirgoang']
+                     WHERE id="District:x3we856001c2ibzweexz"
+             
+    "#;
+    db.query(update_thana).await?;
+
+    //    update number_of_thana according to lenght of police_station lenth
+    let query = r#"
+   UPDATE District
+   SET number_of_thana = array::len(police_station)
+"#;
+    db.query(query).await?;
+
+    // delete record
+    // let query = r#"
+    //     DELETE FROM District
+
+    //     WHERE id="District:2v88h9zbdx9hqc9owr5i"
+    // "#;
+    // db.query(query).await?;
 
     // Delete all records in a table
     // db.delete("khulna").await?;
